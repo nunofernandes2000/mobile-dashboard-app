966a9d1f3dcd744e8867a2f5e5576e39d33133bf
Nuno Fernandes <216351@ipportalegre.pt>
2026-09-07 15:55:48 +0100

    refactor: simplify tokenStorage methods and update file documentation

--- utils/storage.js
+++ utils/storage.js
@@ -1,23 +1,15 @@
-import * as SecureStore from 'expo-secure-store';
+import * as SecureStore from "expo-secure-store";
 
-export const SECURE_KEY_TOKEN = 'ipp_access_token';
-export const KEY_DARK_MODE = 'ipp_dark_mode';
-export const KEY_NAV_TABS = 'ipp_nav_tabs';
-export const KEY_PINNED_SERVICES = 'ipp_pinned_services';
+export const SECURE_KEY_TOKEN = "ipp_access_token";
+export const KEY_DARK_MODE = "ipp_dark_mode";
+export const KEY_NAV_TABS = "ipp_nav_tabs";
+export const KEY_PINNED_SERVICES = "ipp_pinned_services";
 
-/**
- * Utilitário de persistência segura através do SecureStore do Expo
- */
+// Guarda e lê dados no armazenamento seguro do dispositivo (token, preferências, etc.)
 export const tokenStorage = {
-  getItem: async (key) => {
-    return await SecureStore.getItemAsync(key);
-  },
-  setItem: async (key, val) => {
-    await SecureStore.setItemAsync(key, val);
-  },
-  deleteItem: async (key) => {
-    await SecureStore.deleteItemAsync(key);
-  },
+  getItem: (key) => SecureStore.getItemAsync(key),
+  setItem: (key, val) => SecureStore.setItemAsync(key, val),
+  deleteItem: (key) => SecureStore.deleteItemAsync(key),
 };
 
 export default tokenStorage;
